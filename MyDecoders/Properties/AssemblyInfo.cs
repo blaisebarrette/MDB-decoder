9359910f7745fefb755559836a21cad7ffb8af65
Blaise Barrette <53702710+blaisebarrette@users.noreply.github.com>
2025-04-19 00:59:02 -0400

    Update MyDecoders including Properties

--- MyDecoders/Properties/AssemblyInfo.cs
+++ MyDecoders/Properties/AssemblyInfo.cs
@@ -2,17 +2,17 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Protocol Decoders")]
-[assembly: AssemblyDescription("Example decoders for the SmartScope")]
+[assembly: AssemblyTitle("MDB Decoder")]
+[assembly: AssemblyDescription("MDB Decoder for LabNation SmartScope")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("LabNation")]
-[assembly: AssemblyProduct("SmartScope")]
+[assembly: AssemblyCompany("Blaise")]
+[assembly: AssemblyProduct("MDB Decoder")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: ComVisible(false)]
-[assembly: Guid("80a57d6e-02ed-4813-8e8f-2339e1847d10")]
+[assembly: Guid("f8c9e5a7-3b6d-4e12-9a1c-8d5f7e2b6d4c")]
 
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
